0d6889fe685a6283e03dd101f7eeb29da5648c7d
Rainaldo <rcrosbourne@gmail.com>
2021-12-05 13:35:44 -0500

    Updated with escaped apostrophe

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,10 +40,10 @@ const Home: NextPage = () => {
           </div>
           <div className="">
             <p className="font-barlow font-light text-center text-[15px] leading-[25px] text-secondary tracking-wide">
-              Let's face it; if you want to go to space, you might as well
+              Let&apos;s face it; if you want to go to space, you might as well
               genuinely go to outer space and not hover kind of on the edge of
-              it. Well sit back and relax because we'll give you a truly out of
-              this world experience!
+              it. Well sit back and relax because we&apos;ll give you a truly
+              out of this world experience!
             </p>
           </div>
         </div>
