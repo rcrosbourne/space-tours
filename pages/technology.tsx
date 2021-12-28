433196ca10b93a3a6cd5690835fc918c08f3b1b7
Rainaldo <rcrosbourne@gmail.com>
2021-12-28 11:24:23 -0500

    completed the desktop layot

--- pages/technology.tsx
+++ pages/technology.tsx
@@ -69,8 +69,8 @@ const Technology: NextPage = () => {
         />
       </div>
       <Navigation>
-        <div className="flex flex-col items-center justify-between md:grid md:grid-cols-1 xl:grid xl:grid-cols-2 xl:gap-x-[157px] xl:gap-y-16 xl:max-w-[1110.5px] xl:mx-auto">
-          <div className="flex mt-6 md:w-full md:pl-10 md:mt-9 xl:col-span-2 xl:-ml-10 xl:mt-20">
+        <div className="flex flex-col items-center justify-between md:grid md:grid-cols-1 xl:grid xl:grid-cols-3 xl:gap-x-[130px] xl:gap-y-16 xl:max-w-[1110.5px] xl:mx-auto">
+          <div className="flex mt-6 md:w-full md:pl-10 md:mt-9 xl:col-span-3 xl:-ml-10 xl:mt-20">
             <span className="font-barlow-condensed tracking-[2.7px] leading-[19.2px] font-bold text-white opacity-25 md:text-[20px] md:leading-6 md:tracking-[3.38px] xl:text-3xl xl:leading-8 xl:tracking-[4.72px]">
               03
             </span>
@@ -78,21 +78,21 @@ const Technology: NextPage = () => {
               SPACE LAUNCH 101
             </p>
           </div>
-          <div className="relative w-full mt-8 md:mx-auto md:border-none md:w-[456px] md:min-h-[572px] md:mt-[60px]  xl:place-self-end xl:w-[568px] xl:min-h-[712px] xl:row-span-2">
-            <div className="relative w-full min-h-[170px] mx-auto bg-white md:w-full md:h-full">
+          <div className="relative w-full mt-8 md:mx-auto md:min-h-[310px] md:mt-[60px]  xl:place-self-end xl:w-[568px] xl:min-h-[712px] xl:col-start-3 xl:row-start-2 xl:mt-0 xl:row-span-2">
+            <div className="relative w-full min-h-[170px] mx-auto bg-white md:w-full md:min-h-[320px] xl:min-h-[527px]">
               {selectedTechnology.image}
             </div>
           </div>
-          <div className="mt-7 xl:max-w-[445px] md:row-start-2 md:grid md:grid-cols-1 md:mx-auto xl:row-start-2 xl:row-span-2">
-            <div className="flex items-center justify-center xl:justify-start md:row-start-2">
-              <ul className="flex items-center justify-between space-x-4 md:space-x-4 font-barlow-condensed text-sm tracking-[2.36px] leading-[17px] text-white md:tracking-[2.7px] md:leading-[19.2px] md:text-base md:mt-10 xl:mt-30">
+          <div className="mt-7 xl:max-w-[445px] md:grid md:grid-cols-1 md:mx-auto md:mt-14 xl:row-start-2 xl:row-span-1 xl:col-span-2 xl:grid-cols-2 xl:mt-[7.7rem]">
+            <div className="flex items-center justify-center xl:justify-start">
+              <ul className="flex items-center justify-between space-x-4 md:space-x-4 xl:space-y-8 font-barlow-condensed text-sm tracking-[2.36px] leading-[17px] text-white md:tracking-[2.7px] md:leading-[19.2px] md:text-base md:mt-10 xl:mt-0 xl:flex-col xl:space-x-0">
                 {technology.map((tech, index) => (
                   <li key={index}>
                     <button
                       onClick={() => setSelectedTechnology(technology[index])}
                     >
                       <div
-                        className={`flex items-center justify-center mx-auto w-10 h-10 rounded-full xl:w-4 xl:h-4 xl:mt-30 ${
+                        className={`flex items-center font-bellefair text-base leading-[18.34px] md:text-[24px] md:leading-[27.5px] md:tracking-[1.5px] tracking-[1px] justify-center mx-auto w-10 h-10 rounded-full md:w-12 md:h-12 xl:w-20 xl:h-20 xl:mt-30 ${
                           selectedTechnology.id === index
                             ? "bg-white text-primary"
                             : "text-white border-2 border-quaternary"
@@ -105,15 +105,15 @@ const Technology: NextPage = () => {
                 ))}
               </ul>
             </div>
-            <div className="mt-5 md:mt-8 md:max-w-xl xl:mt-16">
+            <div className="mt-5 md:mt-8 md:max-w-[458px] xl:col-start-2 xl:min-w-[470px] xl:mt-0">
               <div className="text-center xl:text-left">
-                <h2 className="text-sm uppercase font-barlow-condensed text-secondary leading-[16.8px] tracking-[2.36px]">
+                <h2 className="text-sm uppercase font-barlow-condensed text-secondary leading-[16.8px] tracking-[2.36px] md:text-base md:leading-[19.2px] md:tracking-[2.7px]">
                   The Terminology...
                 </h2>
-                <p className="uppercase font-bellefair text-2xl mt-2 leading-[27.5px] text-white md:text-2xl md:leading-7 xl:text-4xl xl:leading-9">
+                <p className="uppercase font-bellefair text-2xl mt-2 leading-[27.5px] text-white md:text-4xl md:leading-[45.84px] xl:text-4xl xl:leading-9">
                   {selectedTechnology.title}
                 </p>
-                <p className="font-barlow text-[15px] leading-6 mt-2 text-secondary md:text-[40px] md:leading-[46px] md:mt-2 xl:mt-4 xl:text-[56px] xl:leading-[64px] xl:whitespace-nowrap">
+                <p className="font-barlow text-[15px] leading-6 mt-2 text-secondary md:text-base md:leading-[28px] md:mt-2 xl:mt-4 xl:text-[18px] xl:leading-[32px]">
                   {selectedTechnology.description}
                 </p>
               </div>
